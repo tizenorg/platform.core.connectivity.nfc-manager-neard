6a069899f3d3e1def7d1f389205bbe8924ce0837
Arron Wang <arron.wang@intel.com>
2013-06-28 11:27:22 +0800

    Add tag read/write feature
    Change-Id: Iad70b9860ad53122744fb39176c4fb2cb004e312

--- src/clientlib/include/net_nfc_neard.h
+++ src/clientlib/include/net_nfc_neard.h
@@ -4,6 +4,8 @@
 #include "net_nfc_typedef.h"
 #include "net_nfc_typedef_private.h"
 
+net_nfc_error_e net_nfc_neard_read_tag(net_nfc_target_handle_s *handle, void *trans_param);
+net_nfc_error_e net_nfc_neard_write_ndef(net_nfc_target_handle_s *handle, data_s *data, void *trans_param);
 net_nfc_error_e net_nfc_neard_register_cb(net_nfc_response_cb cb);
 net_nfc_error_e net_nfc_neard_unregister_cb(void);
 net_nfc_error_e net_nfc_neard_is_tag_connected(int *dev_type);
