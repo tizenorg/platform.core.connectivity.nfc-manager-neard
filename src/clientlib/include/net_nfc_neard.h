27535c95325503cb5cca98bac040084b3178ceea
Arron Wang <arron.wang@intel.com>
2013-07-01 09:11:03 +0800

    Add P2P send support
    Change-Id: Ibad62629b7631f2ab315580fc713e902d75b47e1

--- src/clientlib/include/net_nfc_neard.h
+++ src/clientlib/include/net_nfc_neard.h
@@ -6,6 +6,7 @@
 
 net_nfc_error_e net_nfc_neard_read_tag(net_nfc_target_handle_s *handle, void *trans_param);
 net_nfc_error_e net_nfc_neard_write_ndef(net_nfc_target_handle_s *handle, data_s *data, void *trans_param);
+net_nfc_error_e net_nfc_neard_send_p2p(net_nfc_target_handle_s *handle, data_s *data, void *trans_param);
 net_nfc_error_e net_nfc_neard_register_cb(net_nfc_response_cb cb);
 net_nfc_error_e net_nfc_neard_unregister_cb(void);
 net_nfc_error_e net_nfc_neard_is_tag_connected(int *dev_type);
