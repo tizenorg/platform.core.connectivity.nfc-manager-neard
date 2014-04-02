9c040d9750983c0ac8d6498bf1cb476017e4b082
Kévin THIERRY <kevin.thierry@open.eurogiciel.org>
2014-04-02 09:29:06 +0200

    Add multi-user support
    Bug-Tizen: PTREL-378
    Change-Id: I5e417653b721290a65c7c856bbdbde67426bf36a
    Signed-off-by: Kévin THIERRY <kevin.thierry@open.eurogiciel.org>

--- src/manager/include/net_nfc_app_util_private.h
+++ src/manager/include/net_nfc_app_util_private.h
@@ -18,12 +18,15 @@
 #ifndef NET_NFC_APP_UTIL_H
 #define NET_NFC_APP_UTIL_H
 
+/* For multi-user support */
+#include <tzplatform_config.h>
+
 #include "net_nfc_typedef.h"
 #include "net_nfc_typedef_private.h"
 #include <aul.h>
 
 #ifndef MESSAGE_STORAGE
-#define MESSAGE_STORAGE "/opt/share/service/nfc-manager"
+#define MESSAGE_STORAGE tzplatform_mkpath3(TZ_SYS_SHARE, "service", "nfc-manager")
 #endif
 
 net_nfc_error_e net_nfc_app_util_store_ndef_message(data_s *data);
