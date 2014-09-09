6699844c3e2a452c10d2090ddb1a0b3824803ace
Nataliia Koval <nataliia.koval@globallogic.com>
2014-09-09 18:13:49 +0300

    Fixed compilation error: implicit declaration of function 'getpgid'
    Change-Id: I6bf9754c39e93716df56523d96b0c54117669013
    Signed-off-by: Nataliia Koval <nataliia.koval@globallogic.com>

--- client/net_nfc_client_se.c
+++ client/net_nfc_client_se.c
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+ 
+#define _GNU_SOURCE
 #include <vconf.h>
 
 #include "net_nfc_typedef_internal.h"
